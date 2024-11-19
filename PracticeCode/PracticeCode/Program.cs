c6165b885c31441d8bb71ffe8904af0e6d1c03f7
Sakthi-30 <sakthiganesh3010@gmail.com>
2024-11-19 22:50:25 +0530

    Add files via upload

---
+++ PracticeCode/PracticeCode/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = 5;
+for(int i = 1; i <= num; i++)
+{
+    for (int j = 1; j <= num; j++)
+    {
+        if (i == j || i + j == 6)
+        {
+            Console.Write(j + " ");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
